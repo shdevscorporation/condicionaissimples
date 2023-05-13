8fe4af946f33b25cc0348fa9775698104be70e90
HSDEVS CORPORATION <comentandoessebaguio@gmail.com>
2023-05-13 01:15:54 -0300

    first commit

---
+++ simplesmente_if_e_elif.py
@@ -0,0 +1,13 @@
+# condicionais simples if e elif 
+corredor = 'Mario'
+
+# m significa metros
+m = 100
+#80metros para chegada , #50metros para chegada , #100metros concluiu a prova
+
+if m == 20:
+    print('Faltam 80 metros para a chegada...')
+elif m == 50:
+    print('Faltam 50 metros para a chegada...')
+elif m == 100:
+    print(corredor , 'concluiu a prova..')
